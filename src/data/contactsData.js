8d48528b58a2cb8e2fec4ed7c730ab27cd902068
ruw321 <54829988+ruw321@users.noreply.github.com>
2024-01-18 22:08:21 -0600

    Updates

--- src/data/contactsData.js
+++
@@ -1,7 +0,0 @@
-export const contactsData = {
-    email: 'wangruifan1@gmail.com',
-    phone: '+1(631-568-3312)',
-    address: 'Dallas, Texas',
-    github: 'https://github.com/ruw321',
-    linkedIn: 'https://www.linkedin.com/in/wang321',
-}
